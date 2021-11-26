529bdf1eb5f81adc45874809aca86a194a115819
HardyCorentin <93644787+HardyCorentin@users.noreply.github.com>
2021-11-26 09:31:47 +0100

    Add files via upload

---
+++ Projet UI RPG battle/RPG battle JavaScript.js
@@ -0,0 +1,5 @@
+var Mrolov = document.querySelector(.Mrolov).style.backgroundColor = "red";
+var Kima = document.querySelector(.Kima)
+var Lorkos = document.querySelector(.Lorkos)
+var Goure = document.querySelector(.Goure)
+var
