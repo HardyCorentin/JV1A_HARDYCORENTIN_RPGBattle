a9d923a3f3ae73171bb71f343402ac496cccdd8e
HardyCorentin <93644787+HardyCorentin@users.noreply.github.com>
2021-12-08 11:55:08 +0100

    Delete RPG battle JavaScript.js

--- Projet UI RPG battle/RPG battle JavaScript.js
+++
@@ -1,5 +0,0 @@
-var Mrolov = document.querySelector(.Mrolov).style.backgroundColor = "red";
-var Kima = document.querySelector(.Kima)
-var Lorkos = document.querySelector(.Lorkos)
-var Goure = document.querySelector(.Goure)
-var
